1e71323f8a205ea523dc2e35314fae0b082ec9c2
David <DevilDavidWang@gmail.com>
2016-03-20 22:21:59 +0800

    add the user services

---
+++ src/main/java/net/davidvoid/thor/lightning/webservice/UserService.java
@@ -0,0 +1,11 @@
+package net.davidvoid.thor.lightning.webservice;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by david on 3/20/16.
+ */
+@RestController
+public class UserService {
+
+}
