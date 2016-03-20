b05ae4afb29048953f70706e90837bd98343cfd9
David <DevilDavidWang@gmail.com>
2016-03-20 22:23:01 +0800

    add the group entity

---
+++ src/main/java/net/davidvoid/thor/lightning/entity/Group.java
@@ -0,0 +1,25 @@
+package net.davidvoid.thor.lightning.entity;
+
+/**
+ * Created by david on 3/20/16.
+ */
+public class Group {
+    private long id = -1;
+    private String name = "";
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
