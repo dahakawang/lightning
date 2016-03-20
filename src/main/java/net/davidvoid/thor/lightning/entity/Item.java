701d7895fcfa9f95c733dd0015b30cb3e707d7f6
David <DevilDavidWang@gmail.com>
2016-03-20 22:23:22 +0800

    add the item entity

---
+++ src/main/java/net/davidvoid/thor/lightning/entity/Item.java
@@ -0,0 +1,91 @@
+package net.davidvoid.thor.lightning.entity;
+
+import java.util.Date;
+
+/**
+ * Created by david on 3/20/16.
+ */
+public class Item {
+    private long id = -1;
+    private Feed feed = null;
+    private String name = "";
+    private String author = "";
+    private String content = "";
+    private String url = "";
+    private boolean is_saved = false;
+    private boolean is_read = false;
+    private Date last_update = null;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Feed getFeed() {
+        return feed;
+    }
+
+    public void setFeed(Feed feed) {
+        this.feed = feed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public boolean isSaved() {
+        return is_saved;
+    }
+
+    public void setIsSaved(boolean is_saved) {
+        this.is_saved = is_saved;
+    }
+
+    public boolean isRead() {
+        return is_read;
+    }
+
+    public void setIsRead(boolean is_read) {
+        this.is_read = is_read;
+    }
+
+    public Date getLastUpdate() {
+        return last_update;
+    }
+
+    public void setLastUpdate(Date last_update) {
+        this.last_update = last_update;
+    }
+}
