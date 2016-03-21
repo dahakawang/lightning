0f03d3dee78211ac4b28a8b1d103ab4e9e39ea9e
David Wang <DevilDavidWang@gmail.com>
2016-03-21 14:44:35 +0800

    add the config class for data sources

--- src/main/java/net/davidvoid/thor/lightning/config/RootConfig.java
+++ src/main/java/net/davidvoid/thor/lightning/config/RootConfig.java
@@ -1,7 +1,11 @@
 package net.davidvoid.thor.lightning.config;
 
+import net.davidvoid.thor.lightning.data.source.DataSourceConfig;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
+@Import(DataSourceConfig.class)
 public class RootConfig {
 }
---
+++ src/main/java/net/davidvoid/thor/lightning/data/source/DataSourceConfig.java
@@ -0,0 +1,8 @@
+package net.davidvoid.thor.lightning.data.source;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataSourceConfig {
+
+}
