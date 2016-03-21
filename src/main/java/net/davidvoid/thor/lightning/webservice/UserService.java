bb20ee3139283b9806965e5f8882e864d21fbf3e
David Wang <DevilDavidWang@gmail.com>
2016-03-21 13:52:45 +0800

    add a small testing user service

--- src/main/java/net/davidvoid/thor/lightning/webservice/UserService.java
+++ src/main/java/net/davidvoid/thor/lightning/webservice/UserService.java
@@ -1,11 +1,18 @@
 package net.davidvoid.thor.lightning.webservice;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by david on 3/20/16.
  */
 @RestController
+@RequestMapping("/users")
 public class UserService {
-
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String welcome() {
+		return "hello world";
+	}
 }
