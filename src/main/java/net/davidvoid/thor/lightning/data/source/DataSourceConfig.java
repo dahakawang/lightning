e1b8fa7249e3ef339e5502a30febab74b6810366
David <DevilDavidWang@gmail.com>
2016-03-22 23:09:57 +0800

    define a data store for unit test

--- src/main/java/net/davidvoid/thor/lightning/data/source/DataSourceConfig.java
+++ src/main/java/net/davidvoid/thor/lightning/data/source/DataSourceConfig.java
@@ -1,8 +1,16 @@
 package net.davidvoid.thor.lightning.data.source;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class DataSourceConfig {
+
+    @Bean
+    @Profile("test")
+    public MongoDataSource createUnitTest() {
+        return new UnitTestDataSource();
+    }
 
 }
