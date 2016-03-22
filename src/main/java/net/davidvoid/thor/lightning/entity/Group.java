78adfc161b2df5f61914608d32e8ccd30940a15a
David Wang <DevilDavidWang@gmail.com>
2016-03-22 13:28:00 +0800

    groups now have references to the feed ids

--- src/main/java/net/davidvoid/thor/lightning/entity/Group.java
+++ src/main/java/net/davidvoid/thor/lightning/entity/Group.java
@@ -1,13 +1,18 @@
 package net.davidvoid.thor.lightning.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 /**
  * Created by david on 3/20/16.
  */
 public class Group {
     private long id = -1;
     private String name = "";
+    private List<Long> feeds = new ArrayList<Long>();
 
-    public long getId() {
+	public long getId() {
         return id;
     }
 
@@ -21,5 +26,17 @@ public class Group {
 
     public void setName(String name) {
         this.name = name;
+    }
+    
+    public List<Long> getFeeds() {
+		return feeds;
+	}
+
+	public void setFeeds(List<Long> feeds) {
+		this.feeds = feeds;
+	}
+
+	public boolean has_valid_id() {
+    	return id != -1;
     }
 }
