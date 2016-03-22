71a1c4953626b6308e380c04fd078982a11d80ac
David Wang <DevilDavidWang@gmail.com>
2016-03-22 17:46:08 +0800

    update the abstract class

--- src/main/java/net/davidvoid/thor/lightning/data/access/AbstractStore.java
+++ src/main/java/net/davidvoid/thor/lightning/data/access/AbstractStore.java
@@ -9,63 +9,74 @@ import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 
+import net.davidvoid.thor.lightning.data.source.MongoDataSource;
 import net.davidvoid.thor.lightning.entity.Entity;
 import net.davidvoid.thor.lightning.exception.ResourceNotFoundException;
 
 public abstract class AbstractStore {
     @Autowired
+    private MongoDataSource data_source = null;
+
+    @Autowired
     private Counter counter = null;
 
     final public void add(Entity entity) {
-        assert(!entity.has_valid_id());
+        assert (!entity.has_valid_id());
 
         generateId(entity);
         DBObject object = toDBObject(entity);
         getCollection().insert(object);
     }
-    
+
     final public void delete(Entity entity) {
-        assert(entity.has_valid_id());
-        
-        DBObject query = getDeleteQuery(entity);
+        assert (entity.has_valid_id());
+
+        DBObject query = getModifyQuery(entity);
         getCollection().remove(query);
     }
-    
+
     final public void update(Entity entity) {
-        assert(entity.has_valid_id());
-        
-        DBObject query = getUpdateQuery(entity);
+        assert (entity.has_valid_id());
+
+        DBObject query = getModifyQuery(entity);
         DBObject update = toDBObject(entity);
         getCollection().update(query, update, false, true);
     }
-    
 
     final protected List<Entity> get(DBObject query) {
         DBCursor cursor = getCollection().find(query);
-        
+
         ArrayList<Entity> list = new ArrayList<Entity>();
-        while(cursor.hasNext()) {
+        while (cursor.hasNext()) {
             Entity entity = toEntity(cursor.next());
             list.add(entity);
         }
         return list;
     }
-    
+
     final protected Entity getOne(DBObject query) {
         DBObject object = getCollection().findOne(query);
-        if (object == null) throw new ResourceNotFoundException("resource not found in db");
+        if (object == null)
+            throw new ResourceNotFoundException("resource not found in db");
 
         return toEntity(object);
     }
-    
+
     final protected long generateNextId(String name) {
         return counter.getNextId(name);
     }
     
+    final protected DBCollection getCollection(String col_name) {
+        return data_source.getDatabase().getCollection(col_name);
+    }
+
     protected abstract DBObject toDBObject(Entity entity);
+
     protected abstract Entity toEntity(DBObject object);
+
     protected abstract DBCollection getCollection();
+
     protected abstract void generateId(Entity entity);
-    protected abstract DBObject getDeleteQuery(Entity entity);
-    protected abstract DBObject getUpdateQuery(Entity entity);
+
+    protected abstract DBObject getModifyQuery(Entity entity);
 }
