04ffafefb9abc9df88cb8bc03a477861ad5ba558
David Wang <DevilDavidWang@gmail.com>
2016-03-22 17:29:27 +0800

    add exceptions

---
+++ src/main/java/net/davidvoid/thor/lightning/exception/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package net.davidvoid.thor.lightning.exception;
+
+public class ResourceNotFoundException extends ThorException {
+
+    private static final long serialVersionUID = 9049690769685739269L;
+
+    public ResourceNotFoundException(String string) {
+        super(string);
+    }
+
+}
---
+++ src/main/java/net/davidvoid/thor/lightning/exception/ThorException.java
@@ -0,0 +1,11 @@
+package net.davidvoid.thor.lightning.exception;
+
+public class ThorException extends RuntimeException {
+
+    private static final long serialVersionUID = 8473037975359169939L;
+
+    public ThorException(String msg) {
+        super(msg);
+    }
+
+}
