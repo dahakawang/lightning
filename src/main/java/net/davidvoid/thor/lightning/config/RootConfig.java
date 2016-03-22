b18318c9d325432f5926ab96c800e7919ad6eeea
David <DevilDavidWang@gmail.com>
2016-03-22 23:06:52 +0800

    add to scan the accessor package

--- src/main/java/net/davidvoid/thor/lightning/config/RootConfig.java
+++ src/main/java/net/davidvoid/thor/lightning/config/RootConfig.java
@@ -1,11 +1,14 @@
 package net.davidvoid.thor.lightning.config;
 
+import net.davidvoid.thor.lightning.data.access.AbstractStore;
 import net.davidvoid.thor.lightning.data.source.DataSourceConfig;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
 @Import(DataSourceConfig.class)
+@ComponentScan(basePackageClasses = {AbstractStore.class})
 public class RootConfig {
 }
