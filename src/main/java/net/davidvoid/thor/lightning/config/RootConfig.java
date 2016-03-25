a14080e28b1956960fcc44f08560c1ca13337e99
David Wang <DevilDavidWang@gmail.com>
2016-03-25 15:10:35 +0800

    add to auto scan service package

--- src/main/java/net/davidvoid/thor/lightning/config/RootConfig.java
+++ src/main/java/net/davidvoid/thor/lightning/config/RootConfig.java
@@ -2,6 +2,7 @@ package net.davidvoid.thor.lightning.config;
 
 import net.davidvoid.thor.lightning.data.access.AbstractStore;
 import net.davidvoid.thor.lightning.data.source.DataSourceConfig;
+import net.davidvoid.thor.lightning.service.Auth;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +10,6 @@ import org.springframework.context.annotation.Import;
 
 @Configuration
 @Import(DataSourceConfig.class)
-@ComponentScan(basePackageClasses = {AbstractStore.class})
+@ComponentScan(basePackageClasses = {AbstractStore.class, Auth.class})
 public class RootConfig {
 }
