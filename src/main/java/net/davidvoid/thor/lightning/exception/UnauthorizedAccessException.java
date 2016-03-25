1a88fe3bf7c74ba12f823aefd033c22d4d8fdd9f
David Wang <DevilDavidWang@gmail.com>
2016-03-25 17:07:08 +0800

    exception when user did not provide credentials or failed to authenticate...

---
+++ src/main/java/net/davidvoid/thor/lightning/exception/UnauthorizedAccessException.java
@@ -0,0 +1,11 @@
+package net.davidvoid.thor.lightning.exception;
+
+public class UnauthorizedAccessException extends ThorException {
+
+    private static final long serialVersionUID = -9099285304104642354L;
+
+    public UnauthorizedAccessException(String msg) {
+        super(msg);
+    }
+
+}
