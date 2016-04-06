bcc08e1dcbe6e247f7a9eb30dc47eafab1da0628
David Wang <DevilDavidWang@gmail.com>
2016-04-06 15:10:01 +0800

    move the fongo to unit test only. at runtime, we do not need fongo

--- src/main/java/net/davidvoid/thor/lightning/config/RootConfig.java
+++ src/main/java/net/davidvoid/thor/lightning/config/RootConfig.java
@@ -1,15 +1,13 @@
 package net.davidvoid.thor.lightning.config;
 
 import net.davidvoid.thor.lightning.data.access.AbstractStore;
-import net.davidvoid.thor.lightning.data.source.DataSourceConfig;
+import net.davidvoid.thor.lightning.data.source.MongoDataSource;
 import net.davidvoid.thor.lightning.service.Auth;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import(DataSourceConfig.class)
-@ComponentScan(basePackageClasses = {AbstractStore.class, Auth.class})
+@ComponentScan(basePackageClasses = {AbstractStore.class, Auth.class, MongoDataSource.class})
 public class RootConfig {
 }
--- src/test/java/net/davidvoid/thor/lightning/data/source/UnitTestDataSource.java
+++ src/test/java/net/davidvoid/thor/lightning/data/source/UnitTestDataSource.java
@@ -1,5 +1,8 @@
 package net.davidvoid.thor.lightning.data.source;
 
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
 import com.github.fakemongo.Fongo;
 import com.mongodb.DB;
 import com.mongodb.MongoClient;
@@ -7,6 +10,8 @@ import com.mongodb.MongoClient;
 /**
  * Created by david on 3/22/16.
  */
+@Component
+@Profile("test")
 public class UnitTestDataSource implements MongoDataSource {
 
     Fongo fongo = new Fongo("unit_test");
