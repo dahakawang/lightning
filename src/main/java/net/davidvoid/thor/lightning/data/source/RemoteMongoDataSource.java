71d5ebca8be27024f1e5fb529c3184a98c8137af
David Wang <DevilDavidWang@gmail.com>
2016-04-06 18:04:19 +0800

    a mongodb data source connects to a predefined test server

---
+++ src/main/java/net/davidvoid/thor/lightning/data/source/RemoteMongoDataSource.java
@@ -0,0 +1,40 @@
+package net.davidvoid.thor.lightning.data.source;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoDatabase;
+
+@Component
+@Profile("dev-c")
+public class RemoteMongoDataSource implements MongoDataSource {
+    
+    volatile MongoClient mongo = null;
+
+    @Override
+    public MongoClient getClient() {
+        if (mongo != null) return mongo;
+        
+        synchronized(this) {
+            if (mongo != null) return mongo;
+            mongo = createInstance();
+            return mongo;
+        }
+    }
+
+    private MongoClient createInstance() {
+        MongoCredential credentail = MongoCredential.createCredential("admin", "admin", "admin1".toCharArray());
+        return new MongoClient(new ServerAddress("vm-4538-9e7c.nam.nsroot.net:32017"), Arrays.asList(credentail));
+    }
+
+    @Override
+    public MongoDatabase getDatabase() {
+        return mongo.getDatabase("test");
+    }
+
+}
