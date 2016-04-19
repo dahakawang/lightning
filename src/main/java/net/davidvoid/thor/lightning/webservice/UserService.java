8c08ccf1f3f1daa52c3633451c3b6ac3a49e510d
David Wang <DevilDavidWang@gmail.com>
2016-04-19 13:39:47 +0800

    add user services

--- src/main/java/net/davidvoid/thor/lightning/webservice/UserService.java
+++ src/main/java/net/davidvoid/thor/lightning/webservice/UserService.java
@@ -1,5 +1,9 @@
 package net.davidvoid.thor.lightning.webservice;
 
+import java.util.Enumeration;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +16,32 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserService {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String welcome() {
+	public String welcome(HttpServletRequest req) {
+	    Enumeration<String> it = req.getAttributeNames();
+	    while (it.hasMoreElements()) {
+	        String name = it.nextElement();
+	        System.out.println(name + " = " + req.getAttribute(name));
+	    }
 		return "hello world";
+	}
+	
+	@RequestMapping(value = "/{username}", method = RequestMethod.PUT)
+	public String register() {
+	    return null;
+	}
+	
+	@RequestMapping(value = "/{username}/login", method = RequestMethod.POST)
+	public String login() {
+	    return null;
+	}
+	
+	@RequestMapping(value = "/{username}/logout", method = RequestMethod.POST)
+	public String logout() {
+	    return null;
+	}
+	
+	@RequestMapping(value = "/{username}", method = RequestMethod.GET)
+	public String getUser() {
+	    return null;
 	}
 }
