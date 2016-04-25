f002faed39f3c82d62968eadea9c8fb50ba1ad13
David <david.kaiqiang.wang@gmail.com>
2016-04-25 22:20:47 +0800

    return content when pass authentication

--- src/main/java/net/davidvoid/thor/lightning/security/JwtAuthenticationFilter.java
+++ src/main/java/net/davidvoid/thor/lightning/security/JwtAuthenticationFilter.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,9 +18,9 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 
 public class JwtAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
-    
+
     public JwtAuthenticationFilter() {
-        super("/**"); 
+        super("/**");
     }
 
     @Override
@@ -32,7 +33,6 @@ public class JwtAuthenticationFilter extends AbstractAuthenticationProcessingFil
     public Authentication attemptAuthentication(HttpServletRequest request,
             HttpServletResponse response) throws AuthenticationException,
             IOException, ServletException {
-        System.out.println("hehe");
         String header = request.getHeader("Authorization");
         
         if (header == null || !header.startsWith("Bearer ")) {
@@ -43,8 +43,14 @@ public class JwtAuthenticationFilter extends AbstractAuthenticationProcessingFil
         
         List<GrantedAuthority> authorities = new ArrayList<>();
         authorities.add(new SimpleGrantedAuthority("USER"));
-        UsernamePasswordAuthenticationToken user = new UsernamePasswordAuthenticationToken("david", "123"); //, authorities);
-        return null;
+        UsernamePasswordAuthenticationToken user = new UsernamePasswordAuthenticationToken("david", "123", authorities);
+        return user;
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        super.successfulAuthentication(request, response, chain, authResult);
+        chain.doFilter(request, response);
     }
 
 }
