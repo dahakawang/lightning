b4e08383a0dfed649eca4beaf9aeaea4458b9d6a
David <david.kaiqiang.wang@gmail.com>
2016-04-26 22:45:03 +0800

    add implementation

--- src/main/java/net/davidvoid/thor/lightning/security/JwtAuthenticationFilter.java
+++ src/main/java/net/davidvoid/thor/lightning/security/JwtAuthenticationFilter.java
@@ -6,51 +6,56 @@ import java.util.List;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import net.davidvoid.thor.lightning.exception.UnauthorizedAccessException;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.GenericFilterBean;
 
-public class JwtAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
-
-    public JwtAuthenticationFilter(String defaultFilterProcessesUrl) {
-        super(defaultFilterProcessesUrl);
-    }
-
-    protected JwtAuthenticationFilter(
-            RequestMatcher requiresAuthenticationRequestMatcher) {
-        super(requiresAuthenticationRequestMatcher);
-    }
+@Component
+public class JwtAuthenticationFilter extends GenericFilterBean {
 
     @Override
-    public Authentication attemptAuthentication(HttpServletRequest request,
-            HttpServletResponse response) throws AuthenticationException,
-            IOException, ServletException {
-        String header = request.getHeader("Authorization");
-        
-        if (header == null || !header.startsWith("Bearer ")) {
-            throw new BadCredentialsException("");
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+
+        try {
+            Authentication authentication = attemptAuthentication(req, res);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        } catch (UnauthorizedAccessException e) {
+            res.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Authentication failed: " + e.getMessage());
         }
-        
-        String token = header.substring(7);
-        
-        List<GrantedAuthority> authorities = new ArrayList<>();
-        authorities.add(new SimpleGrantedAuthority("USER"));
-        UsernamePasswordAuthenticationToken user = new UsernamePasswordAuthenticationToken("david", "123"); //, authorities);
-        return user;
-    }
 
-    @Override
-    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
-        super.successfulAuthentication(request, response, chain, authResult);
         chain.doFilter(request, response);
     }
 
+    private Authentication attemptAuthentication(HttpServletRequest request,
+                                                 HttpServletResponse response) {
+        String header = request.getHeader("Authorization");
+
+        if (header == null || !header.startsWith("Bearer ")) {
+            throw new UnauthorizedAccessException("no Authorization header found");
+        }
+
+        String token = header.substring(7);
+
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("USER"));
+        UsernamePasswordAuthenticationToken user = new UsernamePasswordAuthenticationToken("david", "123", authorities);
+        return user;
+    }
 }
