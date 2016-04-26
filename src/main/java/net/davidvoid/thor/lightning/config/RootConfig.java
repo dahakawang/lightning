d6925b5bfd86fa14452bfa12b9a0704e0b2fd97c
David <david.kaiqiang.wang@gmail.com>
2016-04-26 22:42:13 +0800

    add secure package to the scan list

--- src/main/java/net/davidvoid/thor/lightning/config/RootConfig.java
+++ src/main/java/net/davidvoid/thor/lightning/config/RootConfig.java
@@ -2,12 +2,13 @@ package net.davidvoid.thor.lightning.config;
 
 import net.davidvoid.thor.lightning.data.access.AbstractStore;
 import net.davidvoid.thor.lightning.data.source.MongoDataSource;
+import net.davidvoid.thor.lightning.security.JwtAuthenticationFilter;
 import net.davidvoid.thor.lightning.service.Auth;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackageClasses = {AbstractStore.class, Auth.class, MongoDataSource.class})
+@ComponentScan(basePackageClasses = {AbstractStore.class, Auth.class, MongoDataSource.class, JwtAuthenticationFilter.class})
 public class RootConfig {
 }
