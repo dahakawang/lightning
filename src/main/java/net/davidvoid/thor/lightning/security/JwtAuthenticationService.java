d2e6043090ac147545b48a45114c21fd09eaab85
David <david.kaiqiang.wang@gmail.com>
2016-04-26 22:45:57 +0800

    add JWT authentication service

---
+++ src/main/java/net/davidvoid/thor/lightning/security/JwtAuthenticationService.java
@@ -0,0 +1,10 @@
+package net.davidvoid.thor.lightning.security;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by david on 4/26/16.
+ */
+@Component
+public class JwtAuthenticationService {
+}
