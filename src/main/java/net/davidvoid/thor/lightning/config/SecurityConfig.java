de23dd62a2e97c602021a02d25c3d0eba5828161
David Wang <DevilDavidWang@gmail.com>
2016-04-27 11:08:57 +0800

    make the login and logout interface public

--- src/main/java/net/davidvoid/thor/lightning/config/SecurityConfig.java
+++ src/main/java/net/davidvoid/thor/lightning/config/SecurityConfig.java
@@ -1,19 +1,11 @@
 package net.davidvoid.thor.lightning.config;
 
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import net.davidvoid.thor.lightning.security.JwtAuthenticationFilter;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
@@ -25,10 +17,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
-        http.authorizeRequests().anyRequest().authenticated()
-                .antMatchers("/users/**").permitAll().and()
-                .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
+        http.authorizeRequests()
+                .antMatchers("/users/*/login").permitAll().antMatchers("/users/*/logout").permitAll()
+                .anyRequest().authenticated()
+                .and().addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
     }
     
 }
