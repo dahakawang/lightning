a2e05f05e5470e796459007f36eb91760b5f60a0
David Wang <DevilDavidWang@gmail.com>
2016-04-27 13:10:54 +0800

    initial implementation of the JWT authentication service

--- src/main/java/net/davidvoid/thor/lightning/service/security/JwtAuthenticationService.java
+++ src/main/java/net/davidvoid/thor/lightning/service/security/JwtAuthenticationService.java
@@ -1,10 +1,21 @@
 package net.davidvoid.thor.lightning.service.security;
 
-import java.security.Key;
-
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.impl.crypto.MacProvider;
+import io.jsonwebtoken.SignatureAlgorithm;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import net.davidvoid.thor.lightning.exception.UnauthorizedAccessException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
@@ -13,12 +24,42 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class JwtAuthenticationService {
-    Key secretKey = null;
+    static Log logger = LogFactory.getLog(JwtAuthenticationService.class);
+
+    String secretKey = null;
+    
+    public JwtAuthenticationService() {
+        InputStream in = this.getClass().getClassLoader().getResourceAsStream("config/key.hmac");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+        try {
+            secretKey = reader.readLine();
+        } catch (IOException e) {
+            logger.fatal("unable to read HMAC key");
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                logger.fatal("failed to close file");
+            }
+        }
+    }
 
     public Authentication authenticate(String jwtToken) {
-        Key secretKey = MacProvider.generateKey();
+        try {
+            Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(jwtToken);
+            String username = claims.getBody().getSubject();
+            if (username == null) throw new UnauthorizedAccessException("no subject present");
 
-        Jwts.parser().setSigningKey(secretKey).parseClaimsJws(jwtToken);
-        return null;
+            UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(username, "");
+            return token;
+
+        } catch (JwtException e) {
+            throw new UnauthorizedAccessException("invalid JWT token", e);
+        }
+    }
+    
+    public String getToken(String username) {
+        return Jwts.builder().setSubject(username).signWith(SignatureAlgorithm.HS512, secretKey).compact();
     }
 }
