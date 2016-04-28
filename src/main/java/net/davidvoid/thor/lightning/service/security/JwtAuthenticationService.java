442f1492b037fd4ed0b8bb7f4b4278082a652e21
David Wang <DevilDavidWang@gmail.com>
2016-04-28 12:14:02 +0800

    now the expiration time is a month (30 days)

--- src/main/java/net/davidvoid/thor/lightning/service/security/JwtAuthenticationService.java
+++ src/main/java/net/davidvoid/thor/lightning/service/security/JwtAuthenticationService.java
@@ -33,7 +33,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class JwtAuthenticationService {
     static Log logger = LogFactory.getLog(JwtAuthenticationService.class);
-    static long EXPIRATION_TIME = 20 * 1000;
+    static long EXPIRATION_TIME = 30 * 24 * 60 * 60* 1000;
 
     String secretKey = null;
     
