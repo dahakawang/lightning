7b8c50cc5f2bac99d7436c3a2af76e332f287402
David Wang <DevilDavidWang@gmail.com>
2016-04-28 12:08:00 +0800

    add token expiration mechanism

--- src/main/java/net/davidvoid/thor/lightning/service/security/JwtAuthenticationService.java
+++ src/main/java/net/davidvoid/thor/lightning/service/security/JwtAuthenticationService.java
@@ -11,9 +11,13 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
+import net.davidvoid.thor.lightning.entity.User;
 import net.davidvoid.thor.lightning.exception.AuthenticationException;
+import net.davidvoid.thor.lightning.util.MapLiteral;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -29,6 +33,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class JwtAuthenticationService {
     static Log logger = LogFactory.getLog(JwtAuthenticationService.class);
+    static long EXPIRATION_TIME = 20 * 1000;
 
     String secretKey = null;
     
@@ -54,6 +59,11 @@ public class JwtAuthenticationService {
             Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(jwtToken);
             String username = claims.getBody().getSubject();
             if (username == null) throw new AuthenticationException("no subject present");
+            
+            Date issuedDate = claims.getBody().getIssuedAt();
+            Date current = new Date();
+            long offset = current.getTime() - issuedDate.getTime();
+            if (offset < 0 || offset > EXPIRATION_TIME) throw new AuthenticationException("token expired");
 
             List<GrantedAuthority> roles = new ArrayList<>();
             roles.add(new SimpleGrantedAuthority("USER"));
@@ -65,7 +75,10 @@ public class JwtAuthenticationService {
         }
     }
     
-    public String getToken(String username) {
-        return Jwts.builder().setSubject(username).signWith(SignatureAlgorithm.HS512, secretKey).compact();
+    public String getToken(User user) {
+        Map<String, Object> additional_claims = MapLiteral.map("uid", user.getId());
+
+        return Jwts.builder().setClaims(additional_claims).setSubject(user.getName()).setIssuedAt(new Date())
+                .signWith(SignatureAlgorithm.HS512, secretKey).compact();
     }
 }
