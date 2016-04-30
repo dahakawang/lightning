1bf7a165319a1bdad379ab6b01d72cf015049c82
David <david.kaiqiang.wang@gmail.com>
2016-04-30 09:15:35 +0800

    ignore the injected User object

--- src/main/java/net/davidvoid/thor/lightning/entity/Group.java
+++ src/main/java/net/davidvoid/thor/lightning/entity/Group.java
@@ -1,11 +1,14 @@
 package net.davidvoid.thor.lightning.entity;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 /**
  * Created by david on 3/20/16.
  */
 public class Group extends Entity {
     private String name = "";
+    @JsonIgnore
     private User user = null;
 
     public String getName() {
