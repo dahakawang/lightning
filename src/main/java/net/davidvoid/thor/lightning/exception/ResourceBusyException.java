7031646a8daa1969dfb8ddd454f6f2e3ae6c6af0
David Wang <DevilDavidWang@gmail.com>
2016-05-06 14:57:43 +0800

    generate serialVersionUID

--- src/main/java/net/davidvoid/thor/lightning/exception/ResourceBusyException.java
+++ src/main/java/net/davidvoid/thor/lightning/exception/ResourceBusyException.java
@@ -4,6 +4,8 @@ package net.davidvoid.thor.lightning.exception;
  * Created by david on 4/30/16.
  */
 public class ResourceBusyException extends BadRequestException {
+    private static final long serialVersionUID = -6219299782402603354L;
+
     public ResourceBusyException() {
         super();
     }
